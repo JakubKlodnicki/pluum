aace77dbee90c0b076ceb0558a61722d167a4e61
Jakub Kłodnicki <96412859+JakubKlodnicki@users.noreply.github.com>
2023-10-16 10:17:43 +0200

    Update - first changes

--- pluum/App.js
+++ pluum/App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
+import React, { FC, ReactElement, useState } from "react";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <><View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/icon.png')} />
+    </View><View style={styles.container2}>
+        <TextInput
+          style={styles.input}
+          value={username}
+          placeholder={"Username"}
+          onChangeText={(text) => setUsername(text)}
+          autoCapitalize={"none"} />
+        <TextInput
+          style={styles.input}
+          value={password}
+          placeholder={"Password"}
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)} />
+        <Button style={styles.button} title={"Sign Up"} onPress={() => { } } />
+        <StatusBar style="auto" />
+      </View></>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  container2: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3535ce',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  container: {
+    backgroundColor: '#3535ce',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 200,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+  },
+  input:placeholder {
+    alignItems: 'center',
+  }
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  button: {
+    color: '#fff',
+  }
 });
